cefe48a22c60d5bf5d0d428de078d1e0296648ff
mdyaseen95 <yaseen.koppal@gmail.com>
2021-09-23 11:22:35 +0530

    Adding for commit

---
+++ src/main/java/com/bridgeLabz/employeewage/EmployeeWage.java
@@ -0,0 +1,8 @@
+package com.bridgeLabz.employeewage;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Calculation Program");
+    }
+}
