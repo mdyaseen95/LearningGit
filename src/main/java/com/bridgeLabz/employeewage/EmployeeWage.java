4a5ddb4ef012e57f699ffcef0d8310c22e50abe9
mdyaseen95 <yaseen.koppal@gmail.com>
2021-09-23 12:19:27 +0530

    Added UC4

--- src/main/java/com/bridgeLabz/employeewage/EmployeeWage.java
+++ src/main/java/com/bridgeLabz/employeewage/EmployeeWage.java
@@ -1,25 +1,27 @@
 package com.bridgeLabz.employeewage;
 
 public class EmployeeWage {
+    public static final int IS_EMPLOYEE_FULL = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int RATE_PER_HOUR = 20;
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Calculation Program");
-        int IS_EMPLOYEE_FULL = 1;
-        int IS_PART_TIME = 2;
-
-        int RATE_PER_HOUR = 20;
-
         int empWage= 0;
         int empHrs = 0;
 
-        double employeecheck = Math.floor(Math.random()*10) % 3;
-        if ( IS_EMPLOYEE_FULL == employeecheck ) {
-            empHrs = 8;
+        int employeecheck = (int) Math.floor(Math.random()*10) % 3;
+        switch ( employeecheck ){
+            case IS_EMPLOYEE_FULL:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
         }
-        else if ( IS_PART_TIME == employeecheck)
-            empHrs = 4;
-        else
-            empHrs = 0;
 
         empWage =empHrs * RATE_PER_HOUR;
         System.out.println("Employee Wage is " +empWage);
