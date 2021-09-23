4435540f8ee51963426ff96c5f35833a897d13ea
mdyaseen95 <yaseen.koppal@gmail.com>
2021-09-23 11:57:36 +0530

    Added UC2

--- src/main/java/com/bridgeLabz/employeewage/EmployeeWage.java
+++ src/main/java/com/bridgeLabz/employeewage/EmployeeWage.java
@@ -4,5 +4,17 @@ public class EmployeeWage {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Calculation Program");
+        int IS_EMPLOYEE_FULL = 1;
+        int RATE_PER_HOUR = 20;
+
+        int empWage= 0;
+        int empHrs = 0;
+
+        double employeecheck = Math.floor(Math.random()*10) % 2;
+        if ( IS_EMPLOYEE_FULL == employeecheck ) {
+            empHrs = 8;
+        }
+        empWage = empHrs * RATE_PER_HOUR;
+        System.out.println("Employee Wage is " +empWage);
     }
 }
