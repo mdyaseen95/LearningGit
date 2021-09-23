17e6360cc9b53101a812a7131f7778358f1fdc3f
mdyaseen95 <yaseen.koppal@gmail.com>
2021-09-23 11:39:38 +0530

    Added UC1

--- src/main/java/com/bridgeLabz/employeewage/EmployeeWage.java
+++ src/main/java/com/bridgeLabz/employeewage/EmployeeWage.java
@@ -4,5 +4,12 @@ public class EmployeeWage {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Calculation Program");
+        int IS_EMPLOYEE_FULL = 1;
+        int employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if (employeeCheck == IS_EMPLOYEE_FULL)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee Not Present");
     }
+
 }
